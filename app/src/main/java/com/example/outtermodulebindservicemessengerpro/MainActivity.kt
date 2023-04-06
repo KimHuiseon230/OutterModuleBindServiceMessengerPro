e26401dd0f2c42bf7273f9673bd9476b33db6987
Kim Huiseon <envy089@gmail.com>
2023-04-06 11:00:40 +0900

    first commit

---
+++ app/src/main/java/com/example/outtermodulebindservicemessengerpro/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.outtermodulebindservicemessengerpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.outtermodulebindservicemessengerpro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
